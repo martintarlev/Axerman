a42d3e8c11b0bebffdd9b1294c145f6143af341b
Martin Tarlev <martin.m.tarlev.2019@elsys-bg.org>
2023-03-06 17:29:31 +0200

    Четири мотора с джойстик на esp 32
    Свързани са четирите мотора и се управляват от едно esp32 свързано с джойстик.

--- Code/Motor_demo.ino
+++ Code/Motor_demo.ino
@@ -1,47 +1,104 @@
-#define in1 25
-#define in2 33
+#define Ain1 25
+#define Ain2 33
+#define Ain3 27
+#define Ain4 26
 #define xjoy 32
+#define Bin1 4
+#define Bin2 16
+#define Bin3 17
+#define Bin4 5 
+#define yjoy 35
 
 int rotDirection = 0;
 int stop = 1;
 
+
 void setup() {
   Serial.begin(115200);
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
+  pinMode(Ain1, OUTPUT);
+  pinMode(Ain2, OUTPUT);
+  pinMode(Ain3, OUTPUT);
+  pinMode(Ain4, OUTPUT);
   pinMode(xjoy, INPUT);
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
+  pinMode(Bin1, OUTPUT);
+  pinMode(Bin2, OUTPUT);
+  pinMode(Bin3, OUTPUT);
+  pinMode(Bin4, OUTPUT);
+  pinMode(yjoy, INPUT);
+  digitalWrite(Ain1, LOW);
+  digitalWrite(Ain2, LOW);
+  digitalWrite(Ain3, LOW);
+  digitalWrite(Ain4, LOW);
+   digitalWrite(Bin1, LOW);
+  digitalWrite(Bin2, LOW);
+  digitalWrite(Bin3, LOW);
+  digitalWrite(Bin4, LOW);
 }
 
 void loop() {
-  int potValue = analogRead(xjoy); // Read potentiometer value
- 
+  int potValue_x = analogRead(xjoy); // Read potentiometer value
+  int potValue_y = analogRead(yjoy); // Read potentiometer value
 
 
-  // If button is pressed - change rotation direction
-  if (potValue < 1500  ) {
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
+
+  if (potValue_x < 1500  ) {
+    digitalWrite(Ain1, HIGH);
+    digitalWrite(Ain2, LOW);
+    digitalWrite(Ain3, HIGH);
+    digitalWrite(Ain4, LOW);
     rotDirection = 1;
     delay(20);
     stop = 1;
   }
-  // If button is pressed - change rotation direction
-  if (potValue > 2200) {
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
+  if (potValue_x > 2200) {
+    digitalWrite(Ain1, LOW);
+    digitalWrite(Ain2, HIGH);
+    digitalWrite(Ain3, LOW);
+    digitalWrite(Ain4, HIGH);
     rotDirection = 0;
     delay(20);
    stop = 1;
 }
- if (potValue < 2200 & potValue > 1500) {
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, LOW);
+ if (potValue_x < 2200 & potValue_x > 1500) {
+    digitalWrite(Ain1, LOW);
+    digitalWrite(Ain2, LOW);
+    digitalWrite(Ain3, LOW);
+    digitalWrite(Ain4, LOW);
     stop = 0;
   
 }
-  Serial.println(potValue);
+
+
+
+
+  if (potValue_y < 1500  ) {
+    digitalWrite(Bin1, HIGH);
+    digitalWrite(Bin2, LOW);
+    digitalWrite(Bin3, HIGH);
+    digitalWrite(Bin4, LOW);
+    rotDirection = 1;
+    delay(20);
+    stop = 1;
+  }
+  if (potValue_y > 2200) {
+    digitalWrite(Bin1, LOW);
+    digitalWrite(Bin2, HIGH);
+    digitalWrite(Bin3, LOW);
+    digitalWrite(Bin4, HIGH);
+    rotDirection = 0;
+    delay(20);
+   stop = 1;
+}
+ if (potValue_y < 2200 & potValue_y > 1500) {
+    digitalWrite(Bin1, LOW);
+    digitalWrite(Bin2, LOW);
+    digitalWrite(Bin3, LOW);
+    digitalWrite(Bin4, LOW);
+    stop = 0;
+  
+}
+  Serial.println(potValue_x);
   Serial.println(stop);
+  Serial.println(potValue_y);
   Serial.println(rotDirection);
 }
